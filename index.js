c22dddf420e3a82da799f4bb441bf676fdcd1e11
Niki2222 <89404731+Niki2222@users.noreply.github.com>
2022-09-11 14:30:18 +0300

    commit4

--- index.js
+++ index.js
@@ -1,39 +1,135 @@
-// function add(a, b) {
-//     return a + b;
-// }
-// function subtract(a, b) {
-//     return a - b;
-// }
-// function amultiply(a, b) {
-//     return a * b;
-// }
-// function divide(a, b) {
-//     return a / b;
-// }
-// function operate() {
+function add(num1, num2) {
+    return num1 + num2
+}
+function subtract(num1, num2) {
+    return num1 - num2
+}
 
-// }
+function multiply(num1, num2) {
+    return num1 * num2
+}
 
-let display = document.getElementById('display');
-let buttons = Array.from(document.getElementsByClassName('button'));
-buttons.map( button => {
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+function operate(op, num1, num2) {
+    switch (op) {
+        case '+':
+            return add(num1, num2)
+        case '-':
+            return subtract(num1, num2)
+        case '*':
+            return multiply(num1, num2)
+        case '/':
+            if (num2 === 0) {
+                buttons.forEach(button => button.setAttribute('disabled', 'true'));
+                zero.setAttribute('disabled', 'true');
+                return 'Infinity';
+            } else {
+                return divide(num1, num2)
+            }
+    }
+}
+
+const buttons = document.querySelectorAll('.btn');
+const clear = document.querySelector('#clear')
+
+clear.addEventListener('click', () => {
+    clearDisplay();
+})
+
+buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        switch(e.target.innerText) {
-            case 'Clear':
-                display.innerText = '';
-                break;
-            case '←':
-                display.innerText = display.innerText.slice(0, -1);
-                break;
-            case '=':
-                try {
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = 'Error!'
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
+        populateNumber(e.target.textContent);
     })
 })
+
+const display = document.getElementById('displayContent');
+let number = '';       
+let operator = null;
+let memory = [];
+function populateNumber(e) {
+    if (parseFloat(e) || e === '0') {
+        number += e;
+        display.textContent = +number;
+    }
+    if (isNaN(parseFloat(e))) {
+        if (number != '' && number !== undefined && number !== null) {
+            switch (e) {
+                case '.':
+                    if (number.includes('.')) {
+                        break;
+                    } else {
+                        number += e
+                        display.textContent = number
+                    }
+                    break;
+                case 'Delete':
+                    number = number.slice(0, -1);
+                    if (number === '') {
+                        number = 0;
+                    }
+                    display.textContent = number;
+                    break;
+                default:
+                    memory.push(number);
+            }
+        }
+        if (memory.length > 2) {
+            memory.splice(0, 1);
+        }
+        switch (e) {
+            case '=':
+                if (memory.length >= 2 && operator != null) {
+                    number = operate(operator, parseFloat(memory[0]), parseFloat(memory[1]));
+                    if (typeof number === 'string') {
+                        display.textContent = number;
+                    } else {
+                        display.textContent = +number.toFixed(4);
+                    }
+                    operator = null;
+                    memory = [];
+                    memory.push(number);
+                    number = '';
+                } else if (operator == null) {
+                    break;
+                } else {
+                    number = '';
+                }
+                break;
+            case '.':
+                break;
+            case 'Delete':
+                break;
+            default:
+                if (memory.length >= 2) {
+                    if (operator == null) {
+                        operator = e;
+                    }
+                    number = operate(operator, parseFloat(memory[0]), parseFloat(memory[1]))
+                    if (typeof number === 'string') {
+                        display.textContent = number;
+                    } else {
+                        display.textContent = +number.toFixed(4);
+                    }
+                    memory = [];
+                    memory.push(number);
+                    number = '';
+                } else {
+                    number = '';
+                }
+                operator = e;
+        }
+    }
+
+}
+
+function clearDisplay() {
+    number = '';
+    memory = [];
+    operator = null;
+    display.textContent = '0';
+    buttons.forEach(button => button.removeAttribute('disabled'));
+    zero.removeAttribute('disabled');
+}
